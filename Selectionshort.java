2092f9ab11904779b1223a5f0a0797a5afd7e8ea
Rudra Pandya <rudrapandyace29@gmail.com>
2024-09-23 23:55:24 +0530

    short

--- Selectionshort.java
+++ Selectionshort.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class Selectionshort {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter total number of Numbers");
         int n = sc.nextInt();
         int[] number = new int [n];
 
+
+        // take input of array 
         for (int i = 0; i < n; i++) {
             System.out.println("Enter "+i+" number");
             number[i] = sc.nextInt();
         }
 
+        // sorting 
         for (int i = 0; i < n-1; i++) {
             int small = i;
             for (int j = i+1; j < n; j++) {
@@ -18,6 +23,12 @@ public class Selectionshort {
                     small = j;
                 }
             }
+            int temp = number[small];
+            number[small] = number[i];
+            number[i]= temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(number[i]+" ");
         }
     }
 }
---
+++ insertionsort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class insertionsort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total number of numbers: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter " + i + " number");
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
